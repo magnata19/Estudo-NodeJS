a9b3b4fcd4fed90ac0087c17aa82df9e3356966a
Davidson Pacifico <dehpacifico@gmail.com>
2023-08-06 14:45:18 -0300

    estudo-node

---
+++ index.js
@@ -0,0 +1,66 @@
+/*
+0 Obter um usuário
+1 Obter o numero de telefone de um usuário a partir de seu Id
+2 Obter o endereco do usuario pelo Id
+*/
+
+function obterUsuario(callback) {
+  setTimeout(function () {
+    return callback(null, {
+      id: 1,
+      nome: "Davidson",
+      dataNascimento: new Date(),
+    });
+  });
+}
+
+function obterTelefone(idUsuario, callback) {
+  setTimeout(() => {
+    return callback(null, {
+      telefone: "12-99182-4569",
+      ddd: 12,
+    });
+  }, 2000);
+}
+
+function obterEndereco(idUsuario, callback) {
+  setTimeout(() => {
+    return callback(null, {
+      rua: "dos bobos",
+      numero: 0,
+    });
+  }, 2000);
+}
+
+function resolverUsuario(erro, usuario) {
+  console.log("usuario", usuario);
+}
+
+obterUsuario(function resolverUsuario(error, usuario) {
+  // se o valor for null || "" || 0 === false
+  if (error) {
+    console.error("DEU RUIM no USUARIO", error);
+    return;
+  }
+
+  obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
+    if (error1) {
+      console.error("DEU RUIM no TELEFONE", error);
+      return;
+    }
+
+    obterEndereco(usuario.id, function resolverEndereco(error2, endereco) {
+      if (error2) {
+        console.error("DEU RUIM no ENDEREÇO", error);
+      }
+
+      console.log(`
+        Nome: ${usuario.nome},
+        Endereço: ${endereco.rua}, ${endereco.numero}
+        Telefone: (${telefone.ddd}) ${telefone.telefone}
+        `);
+    });
+  });
+});
+//const telefone = obterTelefone(usuario.id)
+//console.log('telefone', telefone)
